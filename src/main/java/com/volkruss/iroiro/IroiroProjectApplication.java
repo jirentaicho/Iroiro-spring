260f031b6d5970439d382141dd171e80943c6460
jiren <yujienjoyfever@yahoo.co.jp>
2021-12-08 14:51:36 +0900

    a#

---
+++ src/main/java/com/volkruss/iroiro/IroiroProjectApplication.java
@@ -0,0 +1,13 @@
+package com.volkruss.iroiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IroiroProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IroiroProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volkruss/iroiro/IroiroProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volkruss.iroiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IroiroProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
